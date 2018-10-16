ccd3c287d744ff63f1904fddc6d64e7d35e612ab
Bradley Bell <bradleyb@uw.edu>
2018-10-16 15:37:44 -0700

    cleanup for release

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
-# ems-client
+UW EMS SOAP API Client
+===========================
+
+A client for the EMS SOAP API
+
+Installation
+------------
+
+**Project directory**
+
+Install uw-ems-client in your project.
+
+    $ cd [project]
+    $ pip install UW-EMS-Client
+
+Project settings.py
+------------------
+
+**EMS API settings**
+
+    EMS_API_HOST = '<EMS Server URL>'
+    EMS_API_USERNAME = '<EMS API user>'
+    EMS_API_PASSWORD = '<EMS API password>'
--- setup.py
+++ setup.py
@@ -1,10 +1,19 @@
-from setuptools import find_packages, setup
+import os
+from setuptools import setup
+
+# The VERSION file is created by travis-ci, based on the tag name
+version_path = 'ems_client/VERSION'
+VERSION = open(os.path.join(os.path.dirname(__file__), version_path)).read()
+VERSION = VERSION.replace("\n", "")
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
-    name='ems_client',
-    version='0.1',
-    description='EMS client',
-    packages=find_packages(),
+    name='UW-EMS-Client',
+    version=VERSION,
+    packages=['ems_client'],
+    include_package_data=True,
     install_requires=[
         'commonconf>=0.6',
         'lxml',
@@ -13,5 +22,18 @@ setup(
         'UW-RestClients-Core',
     ],
     license='Apache License, Version 2.0',
+    description='A client for the EMS SOAP API',
+    long_description='README.md',
+    url='https://github.com/uw-it-cte/uw-ems-client',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Office/Business :: Scheduling',
+    ],
     test_suite='runtests.runtests',
 )
